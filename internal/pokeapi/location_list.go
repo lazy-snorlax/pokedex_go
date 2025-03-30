bfabd69983dd41ceefff3e1792a3da9b4565a020
Anthony <blacknebula236@gmail.com>
2025-03-30 15:23:47 +1030

    check cache first before hitting api

--- internal/pokeapi/location_list.go
+++ internal/pokeapi/location_list.go
@@ -2,6 +2,7 @@ package pokeapi
 
 import (
 	"encoding/json"
+	"fmt"
 	"io"
 	"net/http"
 )
@@ -10,6 +11,19 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 	url := baseURL + "/location-area"
 	if pageURL != nil {
 		url = *pageURL
+	}
+
+	locationsResp := RespShallowLocations{}
+
+	cacheEntry, exists := c.cache.Get(url)
+	fmt.Printf("Checking cache - URL: %v  CacheEntry: %v  Exists: %v \n", url, cacheEntry, exists)
+	if exists {
+		fmt.Println("Pulling from cache")
+		err := json.Unmarshal(cacheEntry, &locationsResp)
+		if err != nil {
+			return RespShallowLocations{}, nil
+		}
+		return locationsResp, nil
 	}
 
 	req, err := http.NewRequest("GET", url, nil)
@@ -28,11 +42,11 @@ func (c *Client) ListLocations(pageURL *string) (RespShallowLocations, error) {
 		return RespShallowLocations{}, err
 	}
 
-	locationsResp := RespShallowLocations{}
 	err = json.Unmarshal(dat, &locationsResp)
 	if err != nil {
 		return RespShallowLocations{}, nil
 	}
+	c.cache.Add(url, dat)
 
 	return locationsResp, nil
 }
