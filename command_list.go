3776ba0049bbac2ac98e63f453b36bb3ae508208
Anthony <blacknebula236@gmail.com>
2025-03-31 20:52:13 +1030

    list cmd for party or stored pokemon

---
+++ command_list.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+)
+
+func commandList(cfg *config, args ...string) error {
+	if len(args) != 1 {
+		return errors.New("you must choose either party or pc")
+	}
+
+	name := args[0]
+	if name == "party" {
+		fmt.Println("Your Party:")
+		for _, pokemon := range cfg.partyPokemon {
+			fmt.Printf(" - %s\n", pokemon.Name)
+		}
+	}
+	if name == "pc" {
+		fmt.Println("Your PC:")
+		for _, pokemon := range cfg.storedPokemon {
+			fmt.Printf(" - %s\n", pokemon.Name)
+		}
+	}
+	return nil
+}
